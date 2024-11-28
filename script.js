424b5c204aa491a026ffe169f281a81c167d859a
Ana Julia <anajureis2008@gmail.com>
2024-11-28 11:11:29 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+
+        const button = document.getElementById('location1');
+        
+        // Adiciona um evento de clique
+        button.addEventListener('click', () => {
+            // Redireciona para o Google Maps
+            window.location.href = 'https://www.google.com/maps/place/Pal%C3%A1cio+Real+de+Madrid/@40.4181183,-3.7168977,17z/data=!4m6!3m5!1s0xd42287e7da4a9c1:0x2e7fec79d6ce4851!8m2!3d40.417955!4d-3.714312!16zL20vMDRtM3lr?entry=ttu&g_ep=EgoyMDI0MTEyNC4xIKXMDSoASAFQAw%3D%3D';
+        });
